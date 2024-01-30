dd74a4a36d9c9bc027e11f893e8ca6d4ed4dd7d4
Mansour51 <mansour.zakreev@hotmail.com>
2024-01-30 15:52:26 +0100

    components.1 done

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -6,6 +6,15 @@ function PokemonCard() {
         </figure>
     ) 
 }
-
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
 export default PokemonCard
